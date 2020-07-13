19bccd30ddc3f32ece95ce5c57cef0fa68c05e77
Dan Becker <db19606@gmail.com>
2020-07-12 19:08:48 -0600

    Better font install script; new VM

---
+++ bin/install_fonts.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+# Userland mode (~$USER/), (~/).
+
+# ~/.fonts is now deprecated and
+# ~/.local/share/fonts should be used instead
+FONT_HOME=~/.local/share/fonts
+
+echo "installing fonts to $FONT_HOME"
+mkdir -p "$FONT_HOME/adobe-fonts/source-code-pro"
+mkdir -p "$FONT_HOME/hack"
+
+git clone \
+   --branch release \
+   --depth 1 \
+   'https://github.com/adobe-fonts/source-code-pro.git' \
+   "$FONT_HOME/adobe-fonts/source-code-pro"
+
+git clone https://gitlab.com/protesilaos/hack-font-mod.git $FONT_HOME/hack
+
+sudo fc-cache -f -v "$FONT_HOME"
